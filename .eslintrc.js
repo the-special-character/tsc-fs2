5a30234edd62ffc7c43f7f0a5d4b685c02ab2bbc
Yash Prajapati <yashprajapati7984@gmail.com>
2022-09-01 20:03:10 +0530

    nothing update in this branch

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@babel/eslint-parser",
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};

82c274ab108381a942b05cfb92e5a7b9ee774da1
Jil Sejpal <jilsejpal7@gmail.com>
2022-09-01 15:23:03 +0530

    setup completed

---
+++ app.js
@@ -0,0 +1,3 @@
+import {a, b} from "./app";
+
+const sum = (n1, n2) => n1 + n2;
--- functionHoisting.js
+++
@@ -1,9 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-
-console.log(add(1,2));
-
-function add() {
-    return "hacked..."
-}

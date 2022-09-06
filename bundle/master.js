de41a63f7e5d91cd1295a5f50ff38a2f3c513751
dhruvik shah <devep1210@gmail.com>
2022-09-06 10:47:39 +0530

    setup of webpack config js

---
+++ bundle/master.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(new class{makesound(){console.log("bhow bhow")}}).makesound(),console.log(2)})();
---
+++ index.js
@@ -0,0 +1,18 @@
+import {a,b} from './app';
+
+class Animal{
+    makesound(){
+        console.log("bhow bhow");
+    }
+}
+
+const animal = new Animal;
+animal.makesound();
+
+// something comented
+
+const sum=(n1,n2)=> n1+n2;
+    
+
+
+console.log(sum(a,b));

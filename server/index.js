fbcb7852fa4f7d438346d305af6c36dd0e65db0a
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-09-07 16:57:06 +0530

    completed crud operation

--- server/index.js
+++ server/index.js
@@ -25,26 +25,28 @@ const courses = [
   },
 ];
 
+app.use(express.json());
+
 app.get('/courses', (req, res) => {
-  if (!Object.keys(req.query).length) {
+  const data = Object.keys(req.query);
+  if (data.length === 0) {
     res.send(courses);
-    return;
+    return true;
   }
 
-  const records = courses.filter(course => {
+  const filteredRecord = courses.filter(x => {
     let result = true;
 
-    const arr = Object.keys(req.query);
-
-    for (let i = 0; i < arr.length; i += 1) {
-      const key = arr[i];
-      result = result && course[key] == req.query[key];
+    for (let i = 0; i < data.length; i += 1) {
+      const element = data[i];
+      result &&= x[element] === req.query[element];
     }
 
     return result;
   });
 
-  res.send(records);
+  res.send(filteredRecord);
+  return true;
 });
 
 app.get('/courses/:id', (req, res) => {
@@ -56,6 +58,54 @@ app.get('/courses/:id', (req, res) => {
     res.status(400).send('record not found');
   }
   res.send(record);
+});
+
+app.post('/courses', (req, res) => {
+  console.log(req.body);
+
+  const newData = { ...req.body, id: courses.length + 1 };
+
+  courses.push(newData);
+
+  res.send(newData);
+});
+
+app.put('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = courses.findIndex(x => x.id === Number(id));
+
+  if (index === -1) {
+    res.status(400).send('recod not found');
+  }
+
+  const updatedRecord = { ...req.body, id: Number(id) };
+
+  courses.splice(index, 1, updatedRecord);
+
+  res.send(updatedRecord);
+});
+
+app.patch('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = courses.findIndex(x => x.id === Number(id));
+
+  const updatedRecord = { ...courses[index], ...req.body };
+
+  courses.splice(index, 1, updatedRecord);
+
+  res.send(updatedRecord);
+});
+
+app.delete('/courses/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = courses.findIndex(x => x.id === Number(id));
+
+  courses.splice(index, 1);
+
+  res.send('record deleted');
 });
 
 app.listen('3004', 'localhost', () => {

4f4b9ca32c8b38610715556f7b62283aad26fee8
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-09-09 16:40:15 +0530

    basic setup completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
-
-const app = express();
+const dotenv = require('dotenv');
+const config = require('config');
 const coursesRoute = require('./routes/courses');
 const questionRoute = require('./routes/question');
+
+dotenv.config({
+  override: true,
+});
+
+const dbConfig = config.get('Customer.dbConfig');
+
+console.log(dbConfig);
+
+const app = express();
 
 app.use(express.json());
 
 app.use('/api/courses', coursesRoute);
 app.use('/api/questions', questionRoute);
 
-app.listen('3004', 'localhost', () => {
-  console.log('server started');
+console.log(process.env.AWS_KEY);
+
+const port = process.env.PORT || 3004;
+
+app.listen(port, 'localhost', () => {
+  console.log(`server started on port ${port}`);
 });

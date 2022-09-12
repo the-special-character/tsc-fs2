41b64dd0068f644e5e19ba3396f4b0fb5e195766
dhruvik shah <devep1210@gmail.com>
2022-09-12 10:38:37 +0530

    cleared again

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: "@babel/eslint-parser",
+  
   extends: ["plugin:react/recommended", "airbnb", "prettier"],
   overrides: [],
   parserOptions: {

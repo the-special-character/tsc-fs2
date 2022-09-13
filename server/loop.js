9036f6b9c0e7ec977457361758a2de5d529b95b8
Devang <devang6314@gmail.com>
2022-09-13 11:34:25 +0530

    commit

---
+++ server/loop.js
@@ -0,0 +1,25 @@
+const arr = [1,2,3,4,5,6];
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}
+
+
+
+let i = 0;
+while (i <arr.length) {
+    i++
+}
+
+
+let j = 0;
+do {
+    j++
+} while (i<arr.length);
+
+
+//forEach
+array.forEach(element => {
+    
+});

a3e97ff22c3647306242d0dc39d174301a0de9e4
Yash Prajapati <yashprajapati7984@gmail.com>
2022-09-18 20:06:26 +0530

    user update

--- server/validationSchema/user.js
+++ server/validationSchema/user.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const userSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().lowercase().required(),
   password: Joi.string().min(3).max(16).uppercase(1).lowercase().required(),
 });
 

c904a7167d6a90d920903badf490fe013a1b5864
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-11-02 15:33:39 +0530

    commit code

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,7 @@
 import React, { Component } from 'react';
-import Child2 from './child2';
 
 export default class App extends Component {
-  state = {
-    error: '',
-  };
-
-  static getDerivedStateFromError(error) {
-    return {
-      error,
-    };
-  }
-
-  componentDidCatch(error, info) {
-    console.log(info.componentStack);
-  }
-
   render() {
-    const { error } = this.state;
-    if (error) {
-      return <h1>{error.message}</h1>;
-    }
-
-    return (
-      <div>
-        <Child2 designation="software engineer 1" xyz={5} />
-      </div>
-    );
+    return <div>App</div>;
   }
 }

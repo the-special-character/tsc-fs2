685414548137f235daa78626512307f32779de08
Jil Sejpal <jilsejpal7@gmail.com>
2022-11-03 15:14:46 +0530

    commit code

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,55 @@
-import React, { Component } from 'react';
-import Child2 from './child2';
+import React, { Component } from 'react'
 
 export default class App extends Component {
-    state = {
-        error: '',
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoText: '',
+            todoList: [],
+        };
+
+    }
+
+    // onChangeText = event => {
+    //     this.setState({ todoText: event.target.value });
+    // };
+
+    addTodo = event => {
+        event.preventDefault();
+        this.setState(({ todoList }) => ({
+            todoList: [...todoList, { id: new Date().valueOf(), text: this.todoTextRef.value }],
+
+        }),
+            () => {
+                this.todoTextRef.value = '';
+            },
+        );
     };
 
-    static getDerivedStateFromError(error) {
-        return {
-            error,
-        };
-    }
-
-    componentDidCatch(error, info) {
-        console.log(info.componentStack);
-    }
-
     render() {
-        const { error } = this.state;
-        if (error) {
-            return <h1>{error.message}</h1>;
-        }
-
+        console.log('render');
+        const { todoList } = this.state;
         return (
-            <div>
-                <Child2 designation="software engineer 1" xyz={5} />
-            </div>
-        );
+            <><form onSubmit={this.addTodo}>
+                <div className='flex flex-col items-center '>
+                    <h1 className='text-4xl font-bold my-8'>Todo App</h1>
+                    <input type="text"
+                        ref={ref => {
+                            this.todoTextRef = ref;
+                        }}
+                    // value={todoText} onChange={this.onChangeText} 
+                    />
+                    <button type="button" className='rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>Add Todo</button>
+                </div>
+            </form>
+                <div>
+                    {todoList.map((x) => (
+                        <div key={x.id}>
+                            {x.text}
+                        </div>
+                    ))}
+                </div></>
+        )
     }
 }

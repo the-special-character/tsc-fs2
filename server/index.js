57d3b415d325ff88bbc1302600c4fc69016ea173
Devang Prajapati <200103102134@silveroakuni.ac.in>
2023-02-08 15:41:41 +0530

    Done

--- server/index.js
+++ server/index.js
@@ -1,82 +1,36 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-// const cors = require('cors');
-const passport = require('passport');
+const cors = require('cors');
+const formRoute = require('./routes/form');
 
-const questionRoute = require('./routes/question');
-const userRoute = require('./routes/user.route');
-const batchRoute = require('./routes/batch.route');
 
-dotenv.config({
-  override: true,
-});
+
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use(
-  require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-  }),
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-// app.use(cors());
-
-app.use('/api/auth', userRoute);
-app.use('/api/questions', questionRoute);
-app.use('/api/batch', batchRoute);
-
-const {
-  authenticate,
-  tokenValidation,
-  googleAuthenticate,
-  // serializeUser,
-  // deserializeUser,
-} = require('./auth/authStrategy');
-
-passport.use(authenticate());
-passport.use(tokenValidation());
-passport.use(googleAuthenticate());
-
-passport.serializeUser((user, cb) => {
-  process.nextTick(() => {
-    console.log(user['_json']);
-    cb(
-      null,
-      user['_json'] || {
-        id: user.id,
-        username: user.username,
-        name: user.name,
-      },
-    );
-  });
-});
-
-passport.deserializeUser((user, cb) => {
-  process.nextTick(() => cb(null, user));
-});
+// app.use('/api',questionRoute);
+app.use('/api', formRoute);
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', false);
     await mongoose.connect(
-      'mongodb://localhost:27017/quizeApp',
+      'mongodb://127.0.0.1:27017/Contact',
+      {
+        useNewUrlParser: true,
+      },
     );
-    console.log('database connected....');
+    console.log('database connected...');
   } catch (error) {
     console.log(error);
   }
 };
-
 connectDB();
 
-const port = process.env.PORT || 3004;
+// const port = process.env.PORT || 3004;
 
-app.listen(port, 'localhost', () => {
-  console.log(`server started on port ${port}`);
+app.listen(3004, 'localhost', () => {
+  console.log('server started on port 3004 ');
 });
